f72b2768ddee702c015bd385b1d74c03e7d33609
van_da <iivnnn123@gmail.com>
2024-11-18 02:11:39 -0500

    remove text db

--- functions/utils/db/db.js
+++ functions/utils/db/db.js
@@ -1,6 +1,5 @@
 const mongoose= require("mongoose")
 module.exports= async function connect(){
-    //const connectMongoose= await mongoose.connect("mongodb+srv://Governi1:Governi1@cluster0.nucbabs.mongodb.net/tstingreso?retryWrites=true&w=majority&appName=Cluster0")
     const connectMongoose= await mongoose.connect("mongodb://localhost:27017/tstingreso")
     const connetionM= await mongoose.connection
     connetionM.on("error", (err) => {
